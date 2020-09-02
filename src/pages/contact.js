4debb76bbe6f63a9863f519345b024a0b8cb5593
mwongeraE <evansmwongera@gmail.com>
2020-09-02 13:36:45 +0300

    gatsby docs

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Contact() {
   return (
+      <Layout>
     <div style={{ color: `teal` }}>
       <Link to="/">Home</Link>
       <Header headerText="Contact" />
       <p>Send us a message!</p>
     </div>
+    </Layout>
   )
 }
